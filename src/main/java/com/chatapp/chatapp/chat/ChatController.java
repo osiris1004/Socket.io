9359c896e26e8f30583e5fff76982ab660f6109d
MaesTro <kontchou05@gmail.com>
2024-01-27 19:30:35 +0100

    index 1

---
+++ src/main/java/com/chatapp/chatapp/chat/ChatController.java
@@ -0,0 +1,30 @@
+package com.chatapp.chatapp.chat;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ChatController {
+
+    @MessageMapping("/chat.sendMessage")// end point
+    @SendTo("/topic/public") // mean to which topic or queue we want to send this 
+    public ChatMessage sendMessage(
+            @Payload ChatMessage chatMessage // when we talk about socket we talk about payload
+    ) {
+        return chatMessage;
+    }
+
+    @MessageMapping("/chat.addUser")
+    @SendTo("/topic/public")
+    public ChatMessage addUser(
+            @Payload ChatMessage chatMessage,
+            SimpMessageHeaderAccessor headerAccessor
+    ) {
+        // Add username in web socket session
+        headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
+        return chatMessage;
+    }
+}
---
+++ src/main/java/com/chatapp/chatapp/chat/MessageType.java
@@ -0,0 +1,7 @@
+package com.chatapp.chatapp.chat;
+
+public enum MessageType {
+    CHAT, // if user send message
+    JOIN,  // if user join th chat
+    LEAVE  // if user leave the chat
+}

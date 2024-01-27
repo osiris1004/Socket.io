a4281e1f331c6c1a366c953a0f66d675d863fc68
MaesTro <kontchou05@gmail.com>
2024-01-28 00:15:48 +0100

    index 2

--- src/main/java/com/chatapp/chatapp/chat/MessageType.java
+++
@@ -1,7 +0,0 @@
-package com.chatapp.chatapp.chat;
-
-public enum MessageType {
-    CHAT, // if user send message
-    JOIN,  // if user join th chat
-    LEAVE  // if user leave the chat
-}
--- src/main/java/com/chatapp/chatapp/config/WebSocketConfig.java
+++ src/main/java/com/chatapp/chatapp/config/WebSocketConfig.java
@@ -1,23 +1,31 @@
 package com.chatapp.chatapp.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean;
+import com.chatapp.chatapp.config.handler.ChatHandler;
+
+
 
 @Configuration
-@EnableWebSocketMessageBroker
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
 
     @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").withSockJS();
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(chatHandler(), "/chat")
+                        .setAllowedOrigins("*");
     }
 
-    @Override
-    public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.setApplicationDestinationPrefixes("/app");
-        registry.enableSimpleBroker("/topic");
+    @Bean
+    public WebSocketHandler chatHandler() {
+        return new ChatHandler();
     }
+    
+   
+
 }
